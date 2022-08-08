16fce15700aba5f870c738edc7d31c99a9252ec6
maher <gomemahero.com>
2022-08-08 14:20:17 +0300

    redeploy h to heroku with add socket io

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const http = require("http");
 const app = express();
 const product = require("./api/product");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+
 
 app.use(express.json({ extended: false }));
 app.use(cors());
@@ -12,5 +17,18 @@ app.use("/",  (req, res) => {
     res.send("Hello World");
 } );
 
+
+
+// Socket.io setup
+io.on("connection", socket => {
+    console.log("New client connected");
+    socket.emit("your id", socket.id);
+    socket.on("send message", body => {
+        io.emit("message", body)
+    })
+})
+
+
+
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+server.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
